f22bfecb03897822a7138f69f27208cb010ff69d
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-20 22:30:41 +0530

    SystemUI: Allow to change brightness slider positioning in new compose QS
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57a55c924be7e36d969af4382400e8f0d9415a84
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13852,6 +13852,14 @@ public final class Settings {
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
         /**
+         * Brightness slider position in the quick settings panel
+         * 0 = Top
+         * 1 = Bottom
+         * @hide
+         */
+        public static final String QS_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
