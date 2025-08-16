a50d85a0bb6c1056cb78b49b42121ea12c867084
Pranav Vashi <neobuddy89@gmail.com>
2025-08-16 22:11:32 +0530

    SystemUI: Add tunable for showing brightness icon
    Change-Id: I2806c6d0ae32a5f86632f6e5e86770aaddd75662
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13852,6 +13852,12 @@ public final class Settings {
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
         /**
+         * Whether to show the auto brightness icon in quick settings panel.
+         * @hide
+         */
+        public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
