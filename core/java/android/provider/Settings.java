174a133bbf8fba6a87260ab37ea9c63b9f00b071
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 02:20:19 +0530

    SystemUI: Add customization to select QS tile shape
    * At present it checks only for the state. Inject setting to pre-select shape.
    
    Change-Id: I1a4b8b7977d5d764db2a05215746a84d65cfe3b5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6793,6 +6793,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Customize QS tile shape.
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
