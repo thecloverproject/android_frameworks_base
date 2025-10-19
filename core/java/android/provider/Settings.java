d39519bf7f8e76720a97969b5795996915c461ab
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-19 22:05:26 +0545

    core: Make PIF user configurable [1/2]
    Separated out from [1].
    
    [1]: https://github.com/Neoteric-OS/android_frameworks_base/commit/1fefdf1ea4d74bbe2ff427a499321c5b0959a6cd
    
    Change-Id: Ie86218a5034ff5c6fa9ec3e00a925ffd750686c9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13421,6 +13421,20 @@ public final class Settings {
         public static final String BERRY_BLACK_THEME = "berry_black_theme";
 
         /**
+         * User selectable PIF data.
+         * @hide
+         */
+        @Readable
+        public static final String PIF_DATA = "pif_data";
+
+        /**
+         * PIF data fetched from our repo.
+         * @hide
+         */
+        @Readable
+        public static final String FETCHED_PIF = "fetched_pif";
+
+        /**
          * User selectable keybox data.
          * @hide
          */
