f4899c8525d3f2df9d6e9b7d1d73d78c7f285d4d
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-20 21:02:41 +0800

    base: fixing keyguard slice provider crashes
    01-15 23:00:43.386 E/SliceProviderWrapper(27739): java.lang.NullPointerException: Attempt to read from field 'int com.android.systemui.statusbar.policy.ZenModeControllerImpl.mZenMode' on a null object reference in method 'androidx.slice.Slice com.android.systemui.keyguard.KeyguardSliceProvider.onBindSlice()'
    01-15 23:00:43.386 E/SliceProviderWrapper(27739): 	at com.android.systemui.keyguard.KeyguardSliceProvider.onBindSlice(go/retraceme 4c87fc484271e7594688ec4a5bb534bf6dfa6bc342b28d0b6b4349f4dca58e93:91)
    01-15 23:00:43.386 E/SliceProviderWrapper(27739): 	at androidx.slice.compat.SliceProviderWrapperContainer$SliceProviderWrapper.onBindSlice(go/retraceme
    
    Change-Id: Ic3cff3b2e33f91a2da030ea5dbda3663d2d1418a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -230,7 +230,7 @@ public class KeyguardSliceProvider extends SliceProvider implements
                 addPrimaryActionLocked(builder);
                 slice = builder.build();
             }
-        } catch (IllegalStateException e) {
+        } catch (Exception e) {
             Log.w(TAG, "Could not initialize slice", e);
         } finally {
             Trace.endSection();
